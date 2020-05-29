4e2660c68a886f578fd481611b5fac0cadddefb7
Alexander Plavinski <hello@cilice.me>
2020-05-29 10:54:35 +0200

    Remove gatsby

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Oh.</div>
+</template>
+
+<script lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+})
+</script>
